b2f3f104624ad8f7ccc5ddd1fb22480622da3a0f
Abhishek kumar <abhicrypt97@gmail.com>
2022-06-20 23:41:07 +0530

    complete_hbs_working

--- src/app.js
+++ src/app.js
@@ -2,14 +2,16 @@ const express= require("express");
 const path=require("path");
 const app=express();
 require("./db/conn");
+const Register=require("./models/registers");
 const hbs = require("hbs");
 const port = process.env.PORT || 3000;
 
 const static_path=path.join(__dirname,"../public");
 const template_path= path.join(__dirname,"../templates/views")
 const partial_path= path.join(__dirname,"../templates/partials")
-// const view_login= path.join(__dirname,"../templates/partials")
 
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
 // console.log(path.join(__dirname,"../public"))
 app.use(express.static(static_path));
 
@@ -20,15 +22,39 @@ hbs.registerPartials(partial_path);
 app.get("/",(req,res)=>{
     res.render("index");
 });
-// app.get("/register",(req,res)=>{
-//     res.render("register");
-// });
-// app.get("/login",(req,res)=>{
-//     res.render("login");
-// });
-// app.get("/templates/partials.header",(req,res)=>{
-//     res.render("navbar");
-// })
+app.get("/register",(req,res)=>{
+     res.render("register");
+ });
+// to create new db
+ app.post("/register",async(req,res)=>{
+   try{
+        const password=req.body.password;
+        const cpassword=req.body.confirmpassword;
+        if(password===cpassword)
+        {
+            const registerEmployee= new Register({
+                email:req.body.email,
+                password:password,
+                confirmpassword:cpassword
+            })
+
+            const registered= await  registerEmployee.save();
+            // console.log(registered);
+            res.status(201).render("index");
+        }else{
+            res.send("password not matching");
+        }
+        // console.log(req.body.email);
+        // res.send(req.body.firstname);
+   }catch(error){
+     res.status(400).send(error);
+   }
+});
+ 
+ app.get("/login",(req,res)=>{
+     res.render("login");
+ });
+
 app.listen(port,()=>{
     console.log(`server is running at port no ${port}`);
 })
--- src/models/registers.js
+++ src/models/registers.js
@@ -1,4 +1,19 @@
-// const mongoose =require("mongoose")(
-// const employeeSchema=new mongoose.Schema({
-//     name
-// })
+const mongoose =require("mongoose");
+const employeeSchema=new mongoose.Schema({ 
+    email:{
+        type : String,
+        require:true
+     },
+    password:{
+        type : String,
+        require:true
+    },
+    confirmpassword:{
+         type : String,
+         require:true
+     }
+})
+
+//create collection
+const Register =new mongoose.model("Register",employeeSchema);
+module.exports =Register; 
